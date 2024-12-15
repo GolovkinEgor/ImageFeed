b84d17c86ef7d9126c2eaa3745a59d44b5687ef6
Golovkin Egor <egor4ik1303@mail.ru>
2024-12-16 01:45:53 +0300

    верстка кодом 1 ревью

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -20,7 +20,7 @@ import UIKit
             imageView.tintColor = .gray
             imageView.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview(imageView)
-            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 36).isActive = true
             imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
             imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
@@ -28,49 +28,51 @@ import UIKit
             let label = UILabel()
             label.textColor = .white
             label.text = "Екатерина Новикова"
+            label.font = UIFont.systemFont(ofSize: 23)
             label.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview(label)
             label.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
-            label.topAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+            label.widthAnchor.constraint(equalToConstant: 235).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 18).isActive = true
             self.label = label
             
             let labelTag = UILabel()
             labelTag.textColor = .gray
             labelTag.text = "@ekaterina_nov"
-            label.translatesAutoresizingMaskIntoConstraints = false
+            labelTag.font = UIFont.systemFont(ofSize: 13)
+            labelTag.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview(labelTag)
             labelTag.leadingAnchor.constraint(equalTo: label.leadingAnchor).isActive = true
-            labelTag.topAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
+            labelTag.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 8).isActive = true
+            labelTag.widthAnchor.constraint(equalToConstant: 99).isActive = true
+            labelTag.heightAnchor.constraint(equalToConstant: 18).isActive = true
             self.labelTag = labelTag
             
             let label3 = UILabel()
             label3.textColor = .white
             label3.text = "Hello,world!"
-            label3.translatesAutoresizingMaskIntoConstraints = true
+            label3.font = UIFont.systemFont(ofSize: 13)
+            label3.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview(label3)
             label3.leadingAnchor.constraint(equalTo: labelTag.leadingAnchor).isActive = true
-            label3.topAnchor.constraint(equalTo: labelTag.bottomAnchor).isActive = true
+            label3.topAnchor.constraint(equalTo: labelTag.bottomAnchor, constant: 8).isActive = true
+            label3.widthAnchor.constraint(equalToConstant: 77).isActive = true
+            label3.heightAnchor.constraint(equalToConstant: 18).isActive = true
             self.label3 = label3
             
-            let button = UIButton.systemButton(
-                with: UIImage(named: "LogOut")!,
-                target: self,
-                action: #selector(Self.didTapButton)
-            )
-            button.tintColor = .red
+            
+            
+            let button = UIButton(type: .custom)
+            button.setImage(UIImage(named: "LogOut"), for: .normal)
             button.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview(button)
-            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+            
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
             button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
         }
         
-        @objc
-        private func didTapButton() {
-            
-            label?.removeFromSuperview()
-            label = nil
-            
-        }
+     
     }
 
 
