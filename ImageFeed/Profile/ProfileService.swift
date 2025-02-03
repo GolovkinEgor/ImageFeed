80606dc4e8898cd7fe09ac8d5bad7aec97ed2253
Golovkin Egor <egor4ik1303@mail.ru>
2025-02-04 00:04:46 +0300

    все отображается как должно

--- ImageFeed/Profile/ProfileService.swift
+++ ImageFeed/Profile/ProfileService.swift
@@ -6,6 +6,13 @@ struct ProfileResult: Decodable {
     let firstName: String?
     let lastName: String?
     let bio: String?
+    
+    enum CodingKeys: String, CodingKey {
+          case username
+          case firstName = "first_name"
+          case lastName = "last_name"
+          case bio
+      }
 }
 
 struct Profile {
