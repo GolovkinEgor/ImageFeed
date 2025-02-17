c3f965e2881de18594fdf63ea29e4deb2ff4cfdb
Golovkin Egor <egor4ik1303@mail.ru>
2025-02-17 23:27:48 +0300

    url работает

--- ImageFeed/Profile/ProfileImageService.swift
+++ ImageFeed/Profile/ProfileImageService.swift
@@ -1,11 +1,20 @@
 
 import UIKit
 
-struct UserResult: Decodable{
+struct UserResult: Decodable {
     let profileImage: ProfileImage?
+
+    enum CodingKeys: String, CodingKey {
+        case profileImage = "profile_image"
+    }
 }
+
 struct ProfileImage: Decodable {
     let small: String?
+    
+    enum CodingKeys: String, CodingKey{
+        case small = "small"
+    }
 }
 
 final class ProfileImageService {
